774a9c9a2611575a013e69af6d9518ecfcb24609
Abby2911 <124635236+Abby2911@users.noreply.github.com>
2023-09-29 00:54:49 +0000

    ⭐ Feature: Webpack Middleware

--- server/services/debugLogger.js
+++ server/services/debugLogger.js
@@ -1,6 +1,6 @@
 // Importando dependencia debug
 import Debug from 'debug'
 // Establecemos el proceso a debuggear
-const debug = Debug('projnotes2022b')
+const debug = Debug('dwpcii:server')
 // exportando el logger de debuggeo
 export default debug
