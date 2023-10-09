a922fd3fd5e44dbec8c80f55392f7c357c58e759
Abby2911 <124635236+Abby2911@users.noreply.github.com>
2023-10-09 16:16:57 +0000

    🌟 MaterializeCSS

--- server/bin/www.js
+++ server/bin/www.js
@@ -3,11 +3,16 @@
 /**
  * Module dependencies.
  */
-import Debug from 'debug';
+// Importing the server logic
+// require is used to import code from an external file
+// Importing an external dependecy
+// Module that allows to communicate with a client
+// usign HTTP protocol
 import http from 'http';
 import app from '../app';
 
-const debug = Debug('dwpc2:server');
+// Impornting winston logger
+import log from '../config/winston';
 
 /**
  * Normalize a port into a number, string, or false.
@@ -29,16 +34,19 @@ function normalizePort(val) {
   return false;
 }
 
+/**
+ * Get port from environment and store in Express.
+ */
+
 const port = normalizePort(process.env.PORT || '3000');
+// Store the port info in the app
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
-
-/**
- * Listen on provided port, on all network interfaces.
- */
+log.info('The server is created from the express instance');
+const server = http.createServer(app); // (req, res) => { acciones }
 
 /**
  * Event listener for HTTP server "error" event.
@@ -48,17 +56,15 @@ function onError(error) {
   if (error.syscall !== 'listen') {
     throw error;
   }
-
   const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
-
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(`${bind} requires elevated privileges`);
+      log.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(`${bind} is already in use`);
+      log.error(`${bind} is already in use`);
       process.exit(1);
       break;
     default:
@@ -70,16 +76,16 @@ function onError(error) {
  * Event listener for HTTP server "listening" event.
  */
 
-const server = http.createServer(app); // app tiene la forma (request,response) => {...}
-
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
-  debug(`👂📣Listening on ${bind}`);
+  log.info(`⭐⭐ Listening on ${process.env.APP_URL}:${addr.port} ⭐⭐`);
 }
 
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+// Specifying the port where the server will be listening
 server.listen(port);
-// Registrando eventos del servidor
-// Evento error - variable - funcion callback
+// Attaching Callbacks to events
 server.on('error', onError);
 server.on('listening', onListening);
