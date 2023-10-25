1f87db774775e6122b2e3edc13f3ec699d65510e
Abby2911 <124635236+Abby2911@users.noreply.github.com>
2023-10-25 02:24:39 +0000

    🎯 S03E02 Rutas de Acciones

---
+++ server/domains/project/project.router.js
@@ -0,0 +1,23 @@
+// Importando el Router de Express
+import { Router } from 'express';
+
+// Importando el controlador
+import projectController from './project.controller';
+
+// Creando una isntancia del enrutador
+const router = new Router();
+
+// Enrutamos
+// GET /project/projects
+//  GET /project/dashboard
+router.get(['/projects', '/dashboard'], projectController.showDashboard);
+
+// GET /project/add-form
+// GET /project/add
+router.get(['/add-form', '/add'], projectController.addForm);
+
+// GET /project/about
+router.get('/about', projectController.about);
+
+// Exporto este tramo de ruta
+export default router;
