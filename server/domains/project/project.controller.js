1df8c2d5ad08116dfb5681b4ff9cc0d7c872f051
Abby2911 <124635236+Abby2911@users.noreply.github.com>
2023-11-20 21:01:07 +0000

    🌟 Listando Proyectos

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -1,20 +1,24 @@
-// Importing winston logger
 import log from '../../config/winston';
 
 // Importando el modelo
 import ProjectModel from './project.model';
 
-// Importando Httperrors
+// Action Methods
 
-// Actions methods
-// GET "/project"
-const showDashboard = (req, res) => {
-  res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
+// GET '/project/addForm'
+// GET '/project/add'
+const addForm = (req, res) => {
+  res.render('project/addView');
 };
 
-// GET "/project/add"
-const add = (req, res) => {
-  res.render('project/addView');
+// GET '/project/showDashboard'
+// GET '/project/projects'
+// GET '/project'
+const showDashboard = async (req, res) => {
+  // Consultado todos los proyectos
+  const projects = await ProjectModel.find({}).lean().exec();
+  // Se entrega la vista dashboardView con el viewmodel projects
+  res.render('project/dashboardView', { projects });
 };
 
 // POST "/project/add"
@@ -26,6 +30,7 @@ const addPost = async (req, res) => {
   if (validationError) {
     log.info('Se entrega al cliente error de validación de add Project');
     // Se desestructuran los datos de validación
+    // y se renombran de  "value" a "project"
     const { value: project } = validationError;
     // Se extraen los campos que fallaron en la validación
     const errorModel = validationError.inner.reduce((prev, curr) => {
@@ -42,12 +47,14 @@ const addPost = async (req, res) => {
   // de la peticion
   const { validData: project } = req;
   try {
-    // Creando la instancia de un documento
-    // con los valores de 'project'
+    // Creando la instancia de un documento con los valores de 'project'
     const savedProject = await ProjectModel.create(project);
-    // Se contesta la información del proyecto al cliente
-    log.info('Se entrega al cliente información del proyecto cargado');
-    return res.status(200).json(savedProject);
+    // Se informa al cliente que se guardo el proyecto
+    log.info(`Se carga proyecto ${savedProject}`);
+    // Se registra en el log el redireccionamiento
+    log.info('Se redirecciona el sistema a /project');
+    // Se redirecciona el sistema a la ruta '/project'
+    return res.redirect('/project/showDashboard');
   } catch (error) {
     log.error(
       'ln 53 project.controller: Error al guardar proyecto en la base de datos',
@@ -56,10 +63,8 @@ const addPost = async (req, res) => {
   }
 };
 
-// Controlador user
 export default {
-  // Action Methods
+  addForm,
   showDashboard,
-  add,
   addPost,
 };
